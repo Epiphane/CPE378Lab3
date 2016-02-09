349dd00cdc6d3dc19dcbe0ac5010f34528227765
Epiphane <exyphnos@gmail.com>
2016-02-08 18:11:03 -0800

    Dialog box and writing

--- Lab3v3/CutsceneWorld.java
+++ Lab3v3/CutsceneWorld.java
@@ -28,5 +28,12 @@ public class CutsceneWorld extends World
     {
         CutscenePlayer cutsceneplayer = new CutscenePlayer();
         addObject(cutsceneplayer,262,313);
+        DialogueBox dialoguebox = new DialogueBox();
+        addObject(dialoguebox,617,472);
+        cutsceneplayer.setLocation(124,479);
+        cutsceneplayer.setLocation(527,182);
+        dialoguebox.setLocation(540,421);
+        
+        dialoguebox.say("Hello there!");
     }
 }
